0b6228a56ca314cb80b5076e715d2a6f7357cdd7
moddayz <81351844+moddayz@users.noreply.github.com>
2022-11-09 03:48:26 +0700

    add edit Sound set

--- HelicopterSIB_Hommade_new1/4_World/helicopter.c
+++ HelicopterSIB_Hommade_new1/4_World/helicopter.c
@@ -1,5 +1,15 @@
 class HeliSIB_hommade_new1 extends HeliSIB_hommade
 {
+	override void EEInit()
+	{
+		super.EEInit();
+		c_startSound = "Helicopter_hommade_Start_SoundSets";
+		c_stopSound = "Helicopter_hommade_Stop_SoundSets";
+		c_rotorSound = "Helicopter_hommade_Rotor_SoundSets";
+		c_engineSound = "Helicopter_hommade_Engine_SoundSets";
+		c_warningSound = "Helicopter_hommade_Warning_SoundSets";
+		c_crashSound = "Helicopter_hommade_Crash_SoundSets";
+	}
 	override void creat_InInventory_heli()
 	{
 		if(IsKindOf("HeliSIB_hommade_new1"))
