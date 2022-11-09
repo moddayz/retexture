8bbcaddf555c10725536e74bbc0fe46d8ca81340
moddayz <81351844+moddayz@users.noreply.github.com>
2022-11-09 17:03:31 +0700

    added name SoundShaders

--- HelicopterSIB_Hommade_new1/4_World/helicopter.c
+++ HelicopterSIB_Hommade_new1/4_World/helicopter.c
@@ -3,12 +3,12 @@ class HeliSIB_hommade_new1 extends HeliSIB_hommade
 	override void EEInit()
 	{
 		super.EEInit();
-		c_startSound = "Helicopter_hommade_Start_SoundSets";
-		c_stopSound = "Helicopter_hommade_Stop_SoundSets";
-		c_rotorSound = "Helicopter_hommade_Rotor_SoundSets";
-		c_engineSound = "Helicopter_hommade_Engine_SoundSets";
-		c_warningSound = "Helicopter_hommade_Warning_SoundSets";
-		c_crashSound = "Helicopter_hommade_Crash_SoundSets";
+		c_startSound = "Helicopter_hommade_Start_SoundSets";//Heli_hommade_start_SoundShaders
+		c_stopSound = "Helicopter_hommade_Stop_SoundSets";//Heli_hommade_stop_SoundShaders
+		c_rotorSound = "Helicopter_hommade_Rotor_SoundSets";//Heli_hommade_rotor_SoundShaders
+		c_engineSound = "Helicopter_hommade_Engine_SoundSets";//Heli_hommade_engine_SoundShaders1
+		c_warningSound = "Helicopter_hommade_Warning_SoundSets";//Heli_hommade_warning_SoundShaders
+		c_crashSound = "Helicopter_hommade_Crash_SoundSets";//Heli_hommade_crash_SoundShaders
 	}
 	override void creat_InInventory_heli()
 	{
