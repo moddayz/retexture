4e2ce2f851b4e92108c8c5981f57dcf05b552321
moddayz <81351844+moddayz@users.noreply.github.com>
2023-01-19 06:24:47 +0700

    Update config.cpp

--- HelicopterSIB_UH1D_new1/config.cpp
+++ HelicopterSIB_UH1D_new1/config.cpp
@@ -98,65 +98,60 @@ class CfgVehicles
 	class HeliSIB_UH1D;
 	class HeliSIB_UH1D_clear;
 	
-	
-	
-	
-
-	
 	class HeliSIB_UH1D_new1: HeliSIB_UH1D
 	{
-		scope=2;
-		displayName="UH1D new1";
-		descriptionShort="";
-		attachments[] = {"NivaWheel_1_1","NivaWheel_1_2","NivaWheel_2_1","NivaWheel_2_2","SIBUH1D_Door_1_1","SIBUH1D_Door_1_2","SIBUH1D_Door_2_1","SIBUH1D_Door_2_2","CarBattery","SIBUH1D_blade1","SIBUH1D_blade2","SIBUH1D_bladem1","SIBUH1D_bladem2","Reflector_1_1","Reflector_2_1"};
-		class GUIInventoryAttachmentsProps
-		{	
-			class Chassis
+			scope=2;
+			displayName="UH1D new1";
+			descriptionShort="";
+			attachments[] = {"NivaWheel_1_1","NivaWheel_1_2","NivaWheel_2_1","NivaWheel_2_2","SIBUH1D_Door_1_1","SIBUH1D_Door_1_2","SIBUH1D_Door_2_1","SIBUH1D_Door_2_2","CarBattery","SIBUH1D_blade1","SIBUH1D_blade2","SIBUH1D_bladem1","SIBUH1D_bladem2","Reflector_1_1","Reflector_2_1"};
+			class GUIInventoryAttachmentsProps
 			{
-				name = "$STR_attachment_Chassis0";
-				description = "";
-				icon = "cat_vehicle_chassis";
-				attachmentSlots[] = {"NivaWheel_1_1","NivaWheel_1_2","NivaWheel_2_1","NivaWheel_2_2"};
-			};
-			class Body
+				class Chassis
+				{
+					name = "$STR_attachment_Chassis0";
+					description = "";
+					icon = "cat_vehicle_chassis";
+					attachmentSlots[] = {"NivaWheel_1_1","NivaWheel_1_2","NivaWheel_2_1","NivaWheel_2_2"};
+				};
+				class Body
+				{
+					name = "$STR_attachment_Body0";
+					description = "";
+					icon="cat_vehicle_body";
+					attachmentSlots[] = {"SIBUH1D_Door_1_1","SIBUH1D_Door_1_2","SIBUH1D_Door_2_1","SIBUH1D_Door_2_2","Reflector_1_1","Reflector_2_1"};
+				};
+				class Engine
+				{
+					name = "$STR_attachment_Engine0";
+					description = "";
+					attachmentSlots[] = {"CarBattery","SIBUH1D_blade1","SIBUH1D_blade2","SIBUH1D_bladem1","SIBUH1D_bladem2"};
+					icon = "cat_vehicle_engine";
+				};
+			hiddenSelections[] = {"camo1","camo2"};
+			hiddenSelectionsTextures[] ={"HelicopterSIB_UH1D_new1\data\Uh-1h_bo_airforce_new1.paa","HelicopterSIB_UH1D_new1\data\Uh-1h_do_airforce_new1.paa"};
+			class Cargo 
 			{
-				name = "$STR_attachment_Body0";
-				description = "";
-				icon="cat_vehicle_body";
-				attachmentSlots[] = {"SIBUH1D_Door_1_1","SIBUH1D_Door_1_2","SIBUH1D_Door_2_1","SIBUH1D_Door_2_2","Reflector_1_1","Reflector_2_1"};
+				itemsCargoSize[] = {10,100};
+				allowOwnedCargoManipulation = 1;
+				openable = 0;
 			};
-			class Engine
-			{
-				name = "$STR_attachment_Engine0";
-				description = "";
-				attachmentSlots[] = {"CarBattery","SIBUH1D_blade1","SIBUH1D_blade2","SIBUH1D_bladem1","SIBUH1D_bladem2"};
-				icon = "cat_vehicle_engine";
-			};
-		hiddenSelections[] = {"camo1","camo2"};
-		hiddenSelectionsTextures[] ={"HelicopterSIB_UH1D_new1\data\Uh-1h_bo_airforce_new1.paa","HelicopterSIB_UH1D_new1\data\Uh-1h_do_airforce_new1.paa"};
-		class Cargo 
-		{
-			itemsCargoSize[] = {10,100};
-			allowOwnedCargoManipulation = 1;
-			openable = 0;
 		};
-	};
 
-	
-	class HeliSIB_UH1D_new1_clear: HeliSIB_UH1D_clear
-	{
-		scope=2;
-		displayName="UH1D new1";
-		descriptionShort="";
 		
-		hiddenSelections[] = {"camo1","camo2"};
-		hiddenSelectionsTextures[] ={"HelicopterSIB_UH1D_new1\data\Uh-1h_bo_airforce_new1.paa","HelicopterSIB_UH1D_new1\data\Uh-1h_do_airforce_new1.paa"};
-		class Cargo 
+		class HeliSIB_UH1D_new1_clear: HeliSIB_UH1D_clear
 		{
-			itemsCargoSize[] = {10,100};
-			allowOwnedCargoManipulation = 1;
-			openable = 0;
+			scope=2;
+			displayName="UH1D new1";
+			descriptionShort="";
+			
+			hiddenSelections[] = {"camo1","camo2"};
+			hiddenSelectionsTextures[] ={"HelicopterSIB_UH1D_new1\data\Uh-1h_bo_airforce_new1.paa","HelicopterSIB_UH1D_new1\data\Uh-1h_do_airforce_new1.paa"};
+			class Cargo 
+			{
+				itemsCargoSize[] = {10,100};
+				allowOwnedCargoManipulation = 1;
+				openable = 0;
+			};
 		};
-	};
 	};
 };
